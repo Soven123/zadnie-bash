d18d22ce12a8f27772eeb42023bdb53b5c1ea706
Soven123 <morteusz111@gmail.com>
2021-05-09 18:34:57 +0200

    wyjscie ze skryptu działa i dodano petle while do

--- zad1_loops/zad1.sh
+++ zad1_loops/zad1.sh
@@ -6,49 +6,65 @@ cd test
 #program
 read -p "Jakie pliki wyszukac?: " EXT
 FILES=*.$EXT
+while :
+do
+	echo "Znaleziono nastepujace pliki: "  $FILES
+	echo  Co z nimi zrobic?   
+	echo » 1. Usun
+	echo » 2. Zmien prawa
+	echo » 3. zmien wlasciciela
+	echo » 4. ukryj plik
+	echo » 5. wyjscie
 
-echo "Znaleziono nastepujace pliki: "  $FILES
-echo  Co z nimi zrobic?   
-echo » 1. Usun
-echo » 2. Zmien prawa
-echo » 3. zmien wlasciciela
-echo » 4. ukryj plik
-echo » 5. wyjscie
+	read -p "" CHOICE 
 
-read -p "" CHOICE 
+	if [ $CHOICE = 1 ]
+	then
+		for F in $FILES
+		do
+			rm "$F"
+		done
+		
+		break
+	fi
 
-if [ $CHOICE = 1 ]
-then
-	for F in $FILES
-	do
-		rm "$F"
-	done
-fi
+	if [ $CHOICE = 4 ]
+	then
+		for F in $FILES
+		do
+			mv "$F" ".$F"
+		done
+		
+		break
+	fi
 
-if [ $CHOICE = 4 ]
-then
-	for F in $FILES
-	do
-		mv "$F" ".$F"
-	done
-fi
+	if [ $CHOICE = 3 ]
+	then
+		read -p "Podaj uzytkownika: " USER
+		for F in $FILES
+		do
+			sudo chown $USER "$F"
+		done
+		
+		break
+	fi
 
-if [ $CHOICE = 3 ]
-then
-	read -p "Podaj uzytkownika: " USER
-	for F in $FILES
-	do
-		sudo chown $USER "$F"
-	done
-fi
-
-if [ $CHOICE = 2 ]
-then
-	read -p "Podaj prawa: " PRIVILEGES
-	for F in $FILES
-	do
-		chmod $PRIVILEGES "$F"
-	done
-fi
-
+	if [ $CHOICE = 2 ]
+	then
+		read -p "Podaj prawa: " PRIVILEGES
+		for F in $FILES
+		do
+			chmod $PRIVILEGES "$F"
+		done
+		
+		break
+	fi
+	
+	if [ $CHOICE = 5 ]
+	then
+	
+		break
+	fi
+	
+done
 
